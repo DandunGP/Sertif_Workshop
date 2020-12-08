761f75d7ef21e922e8177f1757208a73dd5ca0aa
Nat <dandun.gigih.prakoso@gmail.com>
2020-12-08 15:50:40 +0700

    Tambah Sertif

---
+++ test.py
@@ -0,0 +1,5 @@
+from Bulk_cert_generator import Cert_gen
+
+Cert_gen.Cert_gen.generate("pdf", "Template.jpeg", "Peserta.xlsx", "Hasil", "CertifWorkshop",{'font_size':1.0 ,'font_color':(0,0,0),'x':80,'y':7})
+
+#Pembuatan Sertif Workshop
